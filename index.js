7a24ed546cc3c4337528fa29c52f7847d5e4903c
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2021-08-09 22:03:03 +0000

    changing the string variables to integers so that it can be higher or lower

--- index.js
+++ index.js
@@ -2,19 +2,37 @@ console.log('Welcome to the high, low card game')
 
 // creating the variable choice options for all the values of cards you can get
 const card = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+const card2 = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 
 play();
 function play(){
 const random = Math.floor(Math.random() * card.length);
 console.log('guess higher or lower than: ' + card[random])
+const random2 = Math.floor(Math.random() * card2.length);
 
-guess = prompt('enter guess')
-if(guess == 'h' || 'higher'){
- console.log('lorem ipsum')
-} else if (guess == 'l' || 'lower'){
-  console.log('lorem ipsum')
-} else {
-  console.log('invalid answer')
-  play();
+// ask the user to guess higher or lower
+// guess = prompt('enter guess')
+// console.log('the card you drew is:' + card2[random2])
+let numberCard = parseInt (card[random])
+let numberCard2 = parseInt (card2[random2]) 
+console.log(numberCard + numberCard2 )
 }
-}
+
+// if(guess == 'h' || 'higher'){
+  
+//   if(card[random] > card2[random2]){
+//     console.log('correct')
+//   }
+//   else if (card[random] < card2[random2]){
+//     console.log('incorrect')
+//   } else {
+//     console.log('incorrect')
+//   }
+ 
+// } else if (guess == 'l' || 'lower'){
+//   console.log('lorem ipsum')
+// } else {
+//   console.log('invalid answer')
+//   play();
+// }
+// }
