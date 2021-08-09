0fd81210dbec366dfcf73628a14561164bfdec56
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2021-08-09 21:42:49 +0000

    the program now asks the player to enter higher or lower but it doesnt do anything with the users answer yet.

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
+console.log('Welcome to the high, low card game')
+
 // creating the variable choice options for all the values of cards you can get
 const card = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
 
-
+play();
+function play(){
 const random = Math.floor(Math.random() * card.length);
-console.log(card[random])
+console.log('guess higher or lower than: ' + card[random])
+
+guess = prompt('enter guess')
+if(guess == 'h' || 'higher'){
+ console.log('lorem ipsum')
+} else if (guess == 'l' || 'lower'){
+  console.log('lorem ipsum')
+} else {
+  console.log('invalid answer')
+  play();
+}
+}
