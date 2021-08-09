5f708897412304c72e664ae5963b6d7039a64983
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2021-08-09 21:31:27 +0000

    i made all the values strings so that there wasn't a mixture of strings and numbers in the const 'card'

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const card = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King' ];
+// creating the variable choice options for all the values of cards you can get
+const card = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+
 
 const random = Math.floor(Math.random() * card.length);
 console.log(card[random])
