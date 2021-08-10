16fe5d28478e281e02fda66bc42aaae5ec210777
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2021-08-10 00:01:32 +0000

    i fixed mistakes in the code. that were causing the the code to not run the 'lowerGuess' function.

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const card2 = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Que
 play();
 function play(){
 const random = Math.floor(Math.random() * card.length);
-console.log('guess higher or lower than: ' + card[random])
 const random2 = Math.floor(Math.random() * card2.length);
+
+
+console.log('guess higher or lower than: ' + card[random])
+
 
 // ask the user to guess higher or lower
 
@@ -17,6 +20,18 @@ let numberCard = parseInt (card[random])
 let numberCard2 = parseInt (card2[random2]) 
 // console.log(numberCard)
 
+if (isNaN(numberCard)){
+  // console.log(card2[random2])
+  if (card[random] == 'Ace'){
+    numberCard = 1
+  } else if (card2[random2] == 'Jack'){
+    numberCard = 11
+  } else if (card2[random2] == 'Queen'){
+    numberCard = 12
+  } else if (card2[random2] == 'King'){
+    numberCard = 13
+  }
+} 
 if (isNaN(numberCard2)){
   // console.log(card2[random2])
   if (card2[random2] == 'Ace'){
@@ -29,33 +44,42 @@ if (isNaN(numberCard2)){
     numberCard2 = 13
   }
 } 
-console.log(numberCard2)
+// console.log(numberCard2)
 
 playerGuess();
+
 function playerGuess(){
-
 guess = prompt('enter guess')
-
-if (guess == 'h' || 'higher'){
+if (guess == 'higher'){
   higherGuess();
-  } else if (guess == 'l' || 'lower'){
+  } else if (guess == 'lower') {
   lowerGuess();
 }
+}
+
 function higherGuess(){
   if (numberCard2 > numberCard){
     console.log('correct')
+    console.log('the card was: ' + card2[random2])
+    ending();
   } else {
     console.log('incorrect')
+    console.log('the card was: ' + card2[random2])
+    ending();
   }
 }
 function lowerGuess(){
-  if (numberCard > numberCard2){
+  if (numberCard2 < numberCard){
     console.log('correct')
+    console.log('the card was: ' + card2[random2])
+    ending();
   } else {
     console.log('incorrect')
+    console.log('the card was: ' + card2[random2])
+    ending();
   }
 }
-}
+
 
 
 
