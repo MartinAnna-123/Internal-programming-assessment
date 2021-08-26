962f0a9327b1f24249ee0d67e47ab85c17444824
Anna Martin <78177123+MartinAnna-123@users.noreply.github.com>
2021-08-26 02:51:51 +0000

    added in some comments

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 // this is the explanation to the players about how the game wos and what to do 
 console.log('Welcome to the high, low card game')
-console.log('This is a two player game. You both have 1 life to get as many points as possible. You can choose to play more rounds when you are both out of lives, and the player with the most points at the end wins')
+console.log('This is a two player game. You both have 1 life to get as many points as possible. The player with the most points at the end wins')
 console.log('Every answer you get correct you get a point but if you draw the same card you will lose a point')
+
+// this is the list of variables that the code will randomly pick out of when the player plays the game.
+const card = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+const card2 = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+
+const player2card = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+const player2card2 = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+
 
 // this loop will keep going until the player enters a whole number that can be used for how many rounds they want to play
 roundLoop = 0
@@ -19,46 +27,36 @@ if (isNaN(rounds)){
 }
 } while (roundLoop < 1)
 
-
-
+// asking the player to enter a name that will be used throughout the game
 player1Name = prompt('Player 1 please enter your name')
 player2Name = prompt('Player 2 please enter your name')
-
-
-
-
-const card = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-const card2 = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-
-const player2card = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-const player2card2 = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
  
+// setting all the variables to the values they need to be for the game to fun effectively (points and lives systems)
 let pointsPlayer1 = 0
 let pointsPlayer2 = 0
 let lives = 0
 let lives2 = 0
 let round = 0
 
+// this is the start of the game. it is functions  in a loop and it will break out of the loop when it has completed the amount of rounds the player entered in the beginning.
 do{
 play1();
 function play1(){
-// getting the program to choose a random value from the list of 'card'
+
+// getting the program to choose a random value from the list of const 'card'
 const random = Math.floor(Math.random() * card.length);
 const random2 = Math.floor(Math.random() * card2.length);
 
 
-// ask the user to guess higher or lower
+// asking the user to guess higher or lower than the randomly picked variable
 console.log( player1Name + ' guess higher or lower than: ' + card[random])
 
 
-
-// console.log('the card you drew is:' + card2[random2])
+//  this section of code will change the strings from the list of variables to numbers. If the string is a word it will change its value to a number. For example 'king' will become 13
 let numberCard = parseInt (card[random])
 let numberCard2 = parseInt (card2[random2]) 
-// console.log(numberCard)
 
 if (isNaN(numberCard)){
-  // console.log(card2[random2])
   if (card[random] == 'Ace'){
     numberCard = 1
   } else if (card[random] == 'Jack'){
@@ -70,8 +68,7 @@ if (isNaN(numberCard)){
   }
 } 
 if (isNaN(numberCard2)){
-  // console.log(card2[random2])
-  if (card2[random2] == 'Ace'){
+ if (card2[random2] == 'Ace'){
     numberCard2 = 1
   } else if (card2[random2] == 'Jack'){
     numberCard2 = 11
@@ -81,9 +78,9 @@ if (isNaN(numberCard2)){
     numberCard2 = 13
   }
 } 
-// console.log(numberCard2)
 
 
+// this function will process the higher or lower answer and determine if it was right or wrong 
 playerGuess1();
 
 function playerGuess1(){
@@ -114,11 +111,7 @@ function higherGuess1(){
     pointsPlayer1 --
     
   }
-   if (lives < 1){
-  play1();
-  } else if (lives >= 1) {
-    play2();
-  }
+   
 }
 function lowerGuess1(){
   if (numberCard2 < numberCard){
@@ -141,27 +134,33 @@ function lowerGuess1(){
   play1();
   } else if (lives >= 1) {
     console.log(player1Name + ', you ran out of lives');
+    console.log('it is now ' + player2Name + 's turn');
     play2();
   }
 }
+// if the player got it wrong they will run out of lives so it will be the next players turn. This 'if' statement determines whether they guess agin or if its the next players turn.
+if (lives < 1){
+  play1();
+  } else if (lives >= 1) {
+    play2();
+  }
 }
 
-
+// the second function with player two. Almost identical to play2(); 
 play2();
 function play2(){
 
-
-    console.log('it is now ' + player2Name + 's turn')
-
+// getting the program to choose a random value from the list of const 'card2'
 const player2random = Math.floor(Math.random() * player2card.length);
 const player2random2 = Math.floor(Math.random() * player2card2.length);
 
+
+//  this section of code will change the strings from the list of variables to numbers. If the string is a word it will change its value to a number. For example 'king' will become 13
 let player2numberCard = parseInt (player2card[player2random])
 let player2numberCard2 = parseInt (player2card2[player2random2]) 
-// console.log(numberCard)
+
 
 if (isNaN(player2numberCard)){
-  // console.log(card2[random2])
   if (player2card[player2random] == 'Ace'){
     player2numberCard = 1
   } else if (player2card[player2random] == 'Jack'){
@@ -173,7 +172,6 @@ if (isNaN(player2numberCard)){
   }
 } 
 if (isNaN(player2numberCard2)){
-  // console.log(card2[random2])
   if (player2card2[player2random2] == 'Ace'){
     numberCard2 = 1
   } else if (player2card2[player2random2] == 'Jack'){
@@ -184,8 +182,11 @@ if (isNaN(player2numberCard2)){
     player2numberCard2 = 13
   }
 } 
+
+// asking the player to pick higher or lower than the randonly selected card from const 'card2'
 console.log( player2Name + ' guess higher or lower than: ' + player2card[player2random])
 
+// this function processes the guess that. the player entered and then refers it to the correct function depending on whether they picked higher or lower.
 playerGuess2();
 function playerGuess2(){
   guess2 = prompt(player2Name + ', enter guess')
@@ -216,11 +217,11 @@ function higherGuess2(){
     pointsPlayer2 --
     
   }
-  if (lives2 < 1){
-    play2();
-  } else if (lives2 >= 1){
-    ending2();
-  }
+  // if (lives2 < 1){
+  //   play2();
+  // } else if (lives2 >= 1){
+  //   ending2();
+  // }
 }
 function lowerGuess2(){
   if (player2numberCard2 < player2numberCard){
@@ -238,22 +239,40 @@ function lowerGuess2(){
     console.log('the card was: ' + player2card2[player2random2])
     pointsPlayer2 --
     
-  }  if (lives2 < 1){
-    round ++
-    play1();
+  } 
+  } 
+  
+  if (lives2 < 1){
+    play2();
   } else if (lives2 >= 1){
     round ++
-  endOfRounds();
-
-  } }
-  
-}} while (round < rounds)
-  
-  function ending1(){
-    console.log(player1Name + ', you ran out of lives');
-    console.log('it is now ' + player2Name + 's turn')
-    play2();
+    }else if (round < rounds){
+      play();
+      
+    } else {
+      break;
+    }
   }
+  
+}
+
+if (pointsPlayer1 > pointsPlayer2){
+    console.log(player1Name + ' wins!, they had ' + pointsPlayer1 + ' points and player 2 had ' + pointsPlayer2 + ' points')
+    
+  }else if (pointsPlayer1 < pointsPlayer2){
+    console.log(player2Name + ' wins!, they had ' + pointsPlayer2 + ' and player 1 had ' + pointsPlayer1 + ' points')
+  } else {
+    console.log('it is a draw. no-one wins')
+  }
+    
+     console.log('the end')
+ 
+ 
+  // function ending1(){
+  //   console.log(player1Name + ', you ran out of lives');
+  //   console.log('it is now ' + player2Name + 's turn')
+  //   play2();
+  // }
   // function ending2(playAgain){
   //   console.log(player2Name + ', you ran out of lives');
   //   console.log('points: ' + pointsPlayer2);
@@ -266,18 +285,9 @@ function lowerGuess2(){
   //     endOfRounds();
   //     }}
 
-    function endOfRounds(){
-     if(pointsPlayer1 > pointsPlayer2){
-      console.log(player1Name + ' wins!, they had ' + pointsPlayer1 + ' points and player 2 had ' + pointsPlayer2 + ' points')
+   
+     
     
-     }else if (pointsPlayer1 < pointsPlayer2){
-      console.log(player2Name + ' wins!, they had ' + pointsPlayer2 + ' and player 1 had ' + pointsPlayer1 + ' points')
-    } else {
-      console.log('it is a draw. no-one wins')
-    }
-    
-     console.log('the end')
-    }
     
   
     
